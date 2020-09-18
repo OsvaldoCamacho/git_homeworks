132f685540f900acf98a6335d0cabd82a00ad9c7
Osvaldo Camacho <luis.cplascencia@alumnos.udg.mx>
2020-09-17 20:02:59 -0500

    Se le cambio la variable lado por lado_cuadrado

--- area_cuadrado.go
+++ area_cuadrado.go
@@ -3,11 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	var lado float64
+	var lado_cuadrado float64
 	fmt.Println("Ingresa el tamaño del lado del cuadrado: ")
-	fmt.Scanf("%f", &lado)
+	fmt.Scanf("%f", &lado_cuadrado)
 
-	res := lado * lado
+	res := lado_cuadrado * lado_cuadrado
 	fmt.Println("El area del cuadrado es: ", res)
 
 }
