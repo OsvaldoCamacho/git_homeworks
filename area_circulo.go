0d69486f94fdc89f19959a47001d9bcd084258cf
Osvaldo Camacho <luis.cplascencia@alumnos.udg.mx>
2020-09-24 12:34:31 -0500

    Comentamos por lo que la funcion va a suplir

--- area_circulo.go
+++ area_circulo.go
@@ -2,16 +2,15 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
 	var radio float64
-	const pi float64 = 3.1416
+
 	fmt.Println("Ingresa el radio del circulo: ")
 	fmt.Scanf("%f", &radio)
 
-	res := (pi * math.Pow(radio, 2))
-	fmt.Println("El area del circulo es: ", res)
+	//res := (pi * math.Pow(radio, 2))
+	//fmt.Println("El area del circulo es: ", res)
 
 }
